492990fba82bad4b0434621a9d3cf3e50e05cb21
moshthepitt <kelvin@jayanoris.com>
2015-07-22 18:12:13 +0300

    improve review mixin

--- reviews/mixins.py
+++ reviews/mixins.py
@@ -7,7 +7,7 @@ from answers.models import Answer
 
 class ReviewMixin(object):
     """
-    Control who can take a peer review
+    Control who can take a review
     """
 
     def dispatch(self, *args, **kwargs):
@@ -18,8 +18,9 @@ class ReviewMixin(object):
                 self.request, messages.WARNING, _('Sorry, you do not have access to that section'))
             return redirect('dashboard')
         # not more than one reviews
-        if review.quiz.single_attempt and Answer.objects.filter(userprofile=self.request.user.userprofile).filter(review=review).exists():
-            messages.add_message(
-                self.request, messages.WARNING, _("You can only review once"))
-            return redirect('dashboard')
+        if review.quiz.single_attempt:
+            if (not self.request.user.is_authenticated()) or Answer.objects.filter(userprofile=self.request.user.userprofile).filter(review=review).exists():
+                messages.add_message(
+                    self.request, messages.WARNING, _("You can only review once"))
+                return redirect('dashboard')
         return super(ReviewMixin, self).dispatch(*args, **kwargs)
