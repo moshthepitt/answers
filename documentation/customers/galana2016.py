4eaa427d739be31f84db1f22a2b50aeeb85177f7
moshthepitt <kelvin@jayanoris.com>
2016-11-28 16:42:11 +0300

    fixes to script

--- documentation/customers/galana2016.py
+++ documentation/customers/galana2016.py
@@ -11,21 +11,17 @@ sitting = Sitting.objects.get(pk=7)
 with open('/srv/jibupro/documentation/customers/galana2016.csv', "rb") as ifile:
     reader = csv.reader(ifile)
     t = zip(reader)
-    for i in t:
-        try:
-            this_user = User.objects.get(email=i[0][0])
-        except User.DoesNotExist:
-            pass
-        else:
-            emails = [x.strip() for x in i[0][1].split(",")]
-            reviewers = User.objects.filter(email__in=emails)
 
-            for quiz in quizzes:
-                this_review = Review(
-                    customer=quiz.customer,
-                    userprofile=this_user.userprofile,
-                    sitting=sitting
-                )
-                this_review.save()
-                for person in reviewers:
-                    this_review.reviewers.add(person.userprofile)
+for i in t:
+    try:
+        this_user = User.objects.get(email=i[0][0])
+    except User.DoesNotExist:
+        pass
+    else:
+        emails = [x.strip() for x in i[0][1].split(",")]
+        reviewers = User.objects.filter(email__in=emails)
+        for quiz in quizzes:
+            this_review = Review(customer=quiz.customer, userprofile=this_user.userprofile, sitting=sitting, quiz=quiz)
+            this_review.save()
+            for person in reviewers:
+                this_review.reviewers.add(person.userprofile)
