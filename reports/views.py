c3975300cbb8dcf1f19b9cfe4d72cd1c7122d772
Kelvin Jayanoris <kjayanoris@gmail.com>
2017-10-23 18:38:07 +0300

    enable individual scores

--- reports/views.py
+++ reports/views.py
@@ -158,7 +158,7 @@ def sitting_report(sitting):
 class ReviewView(CustomerCheckMixin, ReportMixin, DetailView):
     model = Review
     template_name = "reports/review.html"
-    show_individual = False
+    show_individual = True
 
     def get_context_data(self, **kwargs):
         context = super(ReviewView, self).get_context_data(**kwargs)
